6ca5bcf5bfa2075d672632797bc98294f723e24e
CrystalNeko <minecrafthetun@outlook.com>
2023-10-28 18:09:58 +0800

    a

---
+++ ACG.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    //生成随机数
+    var randomNum = Math.floor(Math.random() * (25 - 0 + 1)) + min;
+    //获取链接
+    var link = `https://cdn.jsdelivr.net/gh/CSneko/randomImage@main/ACG/${randomNum}.jpg`
+    //获取&替换元素
+    var div = document.getElementById('randomACG');
+    div.innerHTML = `<img src='${link}' alt='二次元图片'>`;
+})
